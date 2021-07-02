54d970f40cf04adc619dec7d195f48da5ae9d388
rahul ali <82302787+Rahulfordev@users.noreply.github.com>
2021-07-02 18:09:28 +0600

    personal

---
+++ index.js
@@ -0,0 +1,42 @@
+var myAnimation = new hoverEffect({
+  parent: document.querySelector('.create'),
+  intensity: 0.1,
+  image1: 'images/emoji.png',
+  image2: 'images/emoji.png',
+  displacementImage: 'displacement/4.png',
+});
+var myAnimation = new hoverEffect({
+  parent: document.querySelector('.create-two'),
+  intensity: 0.1,
+  image1: 'images/material.png',
+  image2: 'images/material.png',
+  displacementImage: 'displacement/4.png',
+});
+var myAnimation = new hoverEffect({
+  parent: document.querySelector('.create-three'),
+  intensity: 0.1,
+  image1: 'images/todo.png',
+  image2: 'images/todo.png',
+  displacementImage: 'displacement/4.png',
+});
+var myAnimation = new hoverEffect({
+  parent: document.querySelector('.create-four'),
+  intensity: 0.1,
+  image1: 'images/emojis.png',
+  image2: 'images/emojis.png',
+  displacementImage: 'displacement/4.png',
+});
+// const cursor = document.querySelector('.cursor');
+// document.addEventListener('mousemove', (e) => {
+//   cursor.setAttribute(
+//     'style',
+//     'top: ' + (e.pageY - 10) + 'px; left: ' + (e.pageX - 10) + 'px;'
+//   );
+// });
+
+// document.addEventListener('click', () => {
+//   cursor.classList.add('expand');
+//   setTimeout(() => {
+//     cursor.classList.remove('expand');
+//   }, 500);
+// });
